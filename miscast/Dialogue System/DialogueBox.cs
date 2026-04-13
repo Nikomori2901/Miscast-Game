7c0cefa357d92a946a6151d4bb7c67514f7d18a9
Niko <nikomori2901@gmail.com>
2026-04-13 09:58:46 +0800

    Dialogue System
    Dialogue Box appears on passage play displaying passage text.
    Has ability to skip scrolling.
    Continues to next passage on interact, closes when there is no next passage.
    Passages have ability to set portrait image, name & position.

--- miscast/Dialogue System/DialogueBox.cs
+++ miscast/Dialogue System/DialogueBox.cs
@@ -8,5 +8,140 @@ public partial class DialogueBox : Control, IDebuggable
     // --- Export Variables ---
     [Export] public bool debugEnabled { get; set; } = false;
 
+    [Export] private HBoxContainer horizontalContainer;
+    [Export] private Control characterPortraitArea;
+    [Export] private TextureRect characterPortrait;
+    [Export] private Label characterNameLabel;
+    [Export] private RichTextLabel dialogueLabel;
+    [Export] private float scrollSpeed = 30.0f;
+    [Export] private DialoguePassage testPassage;
+
+    // --- Variables ---
+    private DialoguePassage currentPassage;
+    private bool isScrolling;
+    private float scrollProgress;
+
+    #endregion
+
+    #region Events
+
+    #endregion
+
+    #region Inherited Methods
+    // ----- Lifecycle Methods -----
+    public override void _Ready()
+    {
+        DebugManager.DebugPrint(this, "DialogueBox Ready");
+
+        if (testPassage != null)
+        {
+            PlayPassage(testPassage);
+            return;
+        }
+
+        ClearDialogue();
+    }
+
+    public override void _UnhandledInput(InputEvent inputEvent)
+    {
+        if (currentPassage == null)
+        {
+            return;
+        }
+
+        if (inputEvent.IsActionPressed("Continue"))
+        {
+            OnContinue();
+            GetViewport().SetInputAsHandled();
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        if (!isScrolling)
+        {
+            return;
+        }
+
+        scrollProgress += (float)(scrollSpeed * delta);
+        int characterCount = (int)scrollProgress;
+
+        if (characterCount >= dialogueLabel.Text.Length)
+        {
+            dialogueLabel.VisibleCharacters = dialogueLabel.Text.Length;
+            isScrolling = false;
+            return;
+        }
+
+        dialogueLabel.VisibleCharacters = characterCount;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public void PlayPassage(DialoguePassage passage)
+    {
+        currentPassage = passage;
+        Visible = true;
+        DisplayCurrentPassage();
+    }
+
+    public void OnContinue()
+    {
+        if (isScrolling)
+        {
+            SkipScrolling();
+            return;
+        }
+
+        if (currentPassage.nextPassage != null)
+        {
+            currentPassage = currentPassage.nextPassage;
+            DisplayCurrentPassage();
+            return;
+        }
+
+        ClearDialogue();
+    }
+
+    private void DisplayCurrentPassage()
+    {
+        dialogueLabel.Text = currentPassage.dialogueText;
+        characterNameLabel.Text = currentPassage.characterName;
+        characterPortrait.Texture = currentPassage.characterPortrait;
+        ArrangePortraitSide(currentPassage.portraitSide);
+
+        scrollProgress = 0;
+        dialogueLabel.VisibleCharacters = 0;
+        isScrolling = true;
+    }
+
+    private void ArrangePortraitSide(PortraitSide side)
+    {
+        if (side == PortraitSide.Left)
+        {
+            horizontalContainer.MoveChild(characterPortraitArea, 0);
+        }
+
+        else
+        {
+            horizontalContainer.MoveChild(characterPortraitArea, horizontalContainer.GetChildCount() - 1);
+        }
+    }
+
+    private void SkipScrolling()
+    {
+        dialogueLabel.VisibleCharacters = dialogueLabel.Text.Length;
+        isScrolling = false;
+    }
+
+    private void ClearDialogue()
+    {
+        Visible = false;
+        currentPassage = null;
+        isScrolling = false;
+    }
+
     #endregion
 }
